5a1194e16f627fd9aec647acddc89e43c598c9b0
Juan Camilo Rico <juancarico92@gmail.com>
2021-01-12 18:57:54 -0500

    feat: proxy implemented for  getting the dorigin's data

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 class Reactive {
   constructor(options) {
     this.origin = options.data()
+
+    // Destination
+    this.$data = new Proxy(this.origin, {
+      get(target, name) {
+        if (name in target) {
+          return target[name]
+        }
+        console.warn('Property', name, 'doesn\'t exist')
+        return ''
+      }
+    })
   }
 
   mount() {
     document.querySelectorAll('*[p-text]').forEach(el => {
-      this.pText(el, this.origin, el.getAttribute('p-text'))
+      this.pText(el, this.$data, el.getAttribute('p-text'))
     })
   }
 
